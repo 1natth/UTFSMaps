feb0ec7d20cf47070e2cefb933cf6958bc180549
Nathan Cepeda <Nathan.cepeda@usml.cl>
2022-11-28 03:45:47 -0300

    ra

---
+++ UTFSMaps/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(res, res){
+
+    res.send(static_cdn/manage.py)
+})
+
+const PORT = process.env.PORT || 8000
+
+console.log({PORT})
+
+app.listen(PORT, function(){
+
+    console.log("Servidor UTFSMaps", PORT)
+})
+
