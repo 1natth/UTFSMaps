154f1ec4ff608ee6b980ee91cddac42d115c7f46
Nathan Cepeda <Nathan.cepeda@usml.cl>
2022-11-28 01:40:16 -0300

    que paja

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(res, res){
+
+    res.send(C/Users/natha/OneDrive/Escritorio/django/UTFSMaps)
+})
+
+app.listen(8000)
