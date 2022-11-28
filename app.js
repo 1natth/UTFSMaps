df9d29336ce9e0b83db7b05b6aee290d44486400
Nathan Cepeda <Nathan.cepeda@usml.cl>
2022-11-28 18:58:42 -0300

    pag

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs=require('fs');
 const app = express();
 const HTML_CONTENT_TYPE="text/html";
 
-exports.init =('/'),function(req,res){
+app.get ='/',function(req,res){
     res.statusCode = 200;
     res.setHeader('Content-type', HTML_CONTENT_TYPE);
     const index=fs.readFile(`${__dirname}/../libreria/templates/paginas/home.html`, (err,data) =>{
