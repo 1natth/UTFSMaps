3f8313dbc71e7e6973bdd2daf670d59b166de1ee
Nathan Cepeda <Nathan.cepeda@usml.cl>
2022-11-28 18:30:36 -0300

    a

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express()
 
 app.get('/', function(res, res){
 
-    res.sendFile(home.html)
+    res.send('GET request to the homepage')
+})
+app.get('/home.html', function(res, res){
+
+    res.send('home.html')
 })
 
 const PORT = process.env.PORT || 4000
